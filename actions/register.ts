2a8ec442f3e53d2196cd6e0f9cdee72fc4d24609
abhimran <abhimrann@gmail.com>
2025-02-01 01:12:59 +0600

    Implement prisma setup and test user creation

--- actions/register.ts
+++ actions/register.ts
@@ -1,7 +1,9 @@
 "use server";
 
 import { RegisterFormValues, RegisterSchema } from "@/schemas";
-
+import bcrypt from "bcryptjs";
+import { getUserByEmail } from "@/data/users";
+import { db } from "@/lib/db";
 export const registerAc = async (values: RegisterFormValues) => {
   const validateFields = RegisterSchema.safeParse(values);
 
@@ -9,5 +11,17 @@ export const registerAc = async (values: RegisterFormValues) => {
     return { error: "Invalidates field!" };
   }
 
-  return { success: "Email sent! " };
+  const { email, password, name } = validateFields.data;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already exists!" };
+  }
+
+  await db.user.create({ data: { email, password: hashedPassword, name } });
+
+  return { success: "User created!" };
 };
--- lib/db.ts
+++ lib/db.ts
@@ -1,4 +1,3 @@
-// lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
@@ -6,3 +5,5 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export const db = prisma;
